2574a4dce67dafe3b5fe089dd5d81cc5ae7676d4
zhadan93 <zhadan.evgenya@yandex.ru>
2021-03-03 00:28:47 +0300

    fix: towel sort condition

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 module.exports = function towelSort (matrix) {
     let mas = [];
 
-    if (!matrix) return mas;
+    if (!matrix || matrix.length == 0) return mas;
 
     return mas.concat(...matrix.map((item, index) => !(index % 2) ? item : item.reverse()));
 }
